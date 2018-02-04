23841035b8a8eb7c5a5ca6b904cd72356fd63245
Chazie9 <charlesharring3@gmail.com>
2018-02-04 11:46:34 -0800

    level zero working

---
+++ src/components/list_component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import default from '../App';
+
+var ListComponent = (props) => (
+
+    <div>
+        <ul>
+            <div>WOW im HArd CODED</div>
+            <div>WOW im HArd CODED x1</div>
+            <div>WOW im HArd CODED x2</div>
+            <div>WOW im HArd CODED x3</div>
+            <div>WOW im HArd CODED x4</div>
+        </ul>
+    </div>
+);
+
+export default ListComponent;
